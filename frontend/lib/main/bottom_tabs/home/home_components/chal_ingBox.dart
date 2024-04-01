bde348e6c3d716ec57701a011a1cfb4d619fd9aa
HyeEunShin <as221323@kookmin.ac.kr>
2024-04-02 00:30:25 +0900

    [FE/style] home/상단 pupple box존 UI (#23)

---
+++ frontend/lib/main/bottom_tabs/home/home_components/chal_ingBox.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:frontend/model/config/palette.dart';
+
+class ChallengeIngBox extends StatefulWidget {
+  const ChallengeIngBox({super.key});
+
+  @override
+  State<ChallengeIngBox> createState() => _ChallengeIngBoxState();
+}
+
+class _ChallengeIngBoxState extends State<ChallengeIngBox> {
+  String name = "신혜은";
+  String category = "운동";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: PupleBox(),
+    );
+  }
+
+  Widget PupleBox() {
+    return Container(
+      color: Palette.mainPurple,
+      padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                "$name 님 오늘은 $category 관련 \n챌린지를 도전해 볼까요?",
+                // overflow: TextOverflow.visible,
+                // maxLines: null,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Pretendard',
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 15),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all<OutlinedBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12))),
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Palette.white),
+                ),
+                child: Text(
+                  "챌린지 보러가기",
+                  style: TextStyle(
+                      color: Palette.mainPurple,
+                      fontSize: 10,
+                      fontFamily: 'Pretendard',
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(height: 50),
+            ],
+          ),
+          SizedBox(width: 20),
+          Icon(
+            Icons.ice_skating,
+            color: Colors.white,
+            size: 90,
+          )
+        ],
+      ),
+    );
+  }
+}
--- frontend/lib/main/bottom_tabs/home/home_screen.dart
+++ frontend/lib/main/bottom_tabs/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/main/components/home_appBar.dart';
+import 'package:frontend/main/bottom_tabs/home/home_components/chal_ingBox.dart';
+import 'package:frontend/main/bottom_tabs/home/home_components/home_appBar.dart';
 import 'package:frontend/model/config/palette.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -15,7 +16,13 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: PreferredSize(
           preferredSize: Size.fromHeight(80), // 이미지의 높이에 맞춰서 설정
-          child: home_appBar,)
+          child: home_appBar,),
+      body:
+          Column(children: [
+            ChallengeIngBox(),
+            SizedBox(height: 10,)
+          ],)
+
     );
   }
 }
