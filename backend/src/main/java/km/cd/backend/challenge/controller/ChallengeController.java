14749f2b36664e8beace303303789176dd1404e9
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-04-04 11:26:18 +0900

    [backend/fix] RestController로 변경 및 챌린지 참여 기능 추가

--- backend/src/main/java/km/cd/backend/challenge/controller/ChallengeController.java
+++ backend/src/main/java/km/cd/backend/challenge/controller/ChallengeController.java
@@ -1,22 +1,17 @@
 package km.cd.backend.challenge.controller;
 
-import km.cd.backend.challenge.domain.Challenge;
-import km.cd.backend.challenge.dto.ChallengeDto;
+import km.cd.backend.challenge.dto.ChallengeReceivedDto;
 import km.cd.backend.challenge.dto.ChallengeResponseDto;
 import km.cd.backend.challenge.repository.ChallengeRepository;
 import km.cd.backend.challenge.service.ChallengeService;
+import km.cd.backend.user.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class ChallengeController {
 
     @Autowired
@@ -30,25 +25,16 @@ public class ChallengeController {
         this.challengeService = challengeService;
     }
 
-    @GetMapping("/challenge/create_form")
-    public String newChallengeForm() {
-        return "/challenge/create_form2";
+    @PostMapping("/challenge/create")
+    public ChallengeResponseDto createChallenge(@ModelAttribute ChallengeReceivedDto challengeReceivedDTO, @AuthenticationPrincipal User user) {
+        ChallengeResponseDto saved = challengeService.createChallenge(challengeReceivedDTO, user);
+        return saved;
     }
 
-    @PostMapping("/challenge/create_form")
-    public String createChallenge(@ModelAttribute ChallengeDto challengeDTO, RedirectAttributes redirectAttributes) {
-        log.info(challengeDTO.toString());
-        ChallengeResponseDto saved = challengeService.saveChallenge(challengeDTO);
-        log.info(saved.toString());
-        redirectAttributes.addAttribute("challenge_id", saved.getChallengeId());
-        return "redirect:/challenge/success";
+    @PostMapping("/challenge/{challenge_id}/join")
+    public String joinChallenge(@PathVariable int challenge_id, @AuthenticationPrincipal User user) {
+        challengeService.joinChallenge(challenge_id, user);
+        return "Success";
     }
-
-    @GetMapping("/challenge/success")
-    public String showSuccessPage(@RequestParam Integer challenge_id, Model model) {
-        model.addAttribute("challenge_id", challenge_id);
-        return "/challenge/success2";
-    }
-
 
 }
