d1c6bab801cc3059835f308d0c08c7ea09f0ccac
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-04-04 12:53:20 +0900

    [backend/fix] id 타입 변경 및 participants 속성 추가에 따른 DTO 수정

--- backend/src/main/java/km/cd/backend/challenge/dto/ChallengeResponseDto.java
+++ backend/src/main/java/km/cd/backend/challenge/dto/ChallengeResponseDto.java
@@ -1,13 +1,15 @@
 package km.cd.backend.challenge.dto;
 
 import km.cd.backend.challenge.domain.Challenge;
+import km.cd.backend.challenge.domain.Participant;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.List;
 
 @Data
 public class ChallengeResponseDto {
-    private Integer challengeId;
+    private Long challengeId;
     private String challengeName;
     private Date startDate;
     private Date endDate;
@@ -19,13 +21,14 @@ public class ChallengeResponseDto {
     private Integer maximumPeople;
     private Boolean isPrivate;
     private String privateCode;
+    private List<Participant> participants;
 
     public ChallengeResponseDto() {
     }
 
-    public ChallengeResponseDto(Integer challengeId, String challengeName, Date startDate, Date endDate, Integer certificationFrequency,
-                                String certificationExplanation, Integer certificationCount, String certificationMethod,
-                                String challengeExplanation, Integer maximumPeople, Boolean isPrivate, String privateCode) {
+    public ChallengeResponseDto(Long challengeId, String challengeName, Date startDate, Date endDate, Integer certificationFrequency,
+        String certificationExplanation, Integer certificationCount, String certificationMethod,
+        String challengeExplanation, Integer maximumPeople, Boolean isPrivate, String privateCode, List<Participant> participants) {
         this.challengeId = challengeId;
         this.challengeName = challengeName;
         this.startDate = startDate;
@@ -38,21 +41,24 @@ public class ChallengeResponseDto {
         this.maximumPeople = maximumPeople;
         this.isPrivate = isPrivate;
         this.privateCode = privateCode;
+        this.participants = participants;
     }
+
     public static ChallengeResponseDto toDto(Challenge challenge) {
         return new ChallengeResponseDto(
-                challenge.getChallenge_id(),
-                challenge.getChallenge_name(),
-                challenge.getStart_date(),
-                challenge.getEnd_date(),
-                challenge.getCertification_frequency(),
-                challenge.getCertification_explanation(),
-                challenge.getCertification_count(),
-                challenge.getCertification_method(),
-                challenge.getChallenge_explanation(),
-                challenge.getMaximum_people(),
-                challenge.getIs_private(),
-                challenge.getPrivate_code()
+            challenge.getChallenge_id(),
+            challenge.getChallenge_name(),
+            challenge.getStart_date(),
+            challenge.getEnd_date(),
+            challenge.getCertification_frequency(),
+            challenge.getCertification_explanation(),
+            challenge.getCertification_count(),
+            challenge.getCertification_method(),
+            challenge.getChallenge_explanation(),
+            challenge.getMaximum_people(),
+            challenge.getIs_private(),
+            challenge.getPrivate_code(),
+            challenge.getParticipants()
         );
     }
 }
