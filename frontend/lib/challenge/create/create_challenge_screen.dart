9d6a3fff3c0a8b22be1bed3cc04101e25b36fb4b
HyeEunShin <as221323@kookmin.ac.kr>
2024-04-05 13:49:11 +0900

    [FE/style] body_part3 : ADD 인증방법 TextEdit, 인증수단 picker

--- frontend/lib/challenge/create/create_challenge_screen.dart
+++ frontend/lib/challenge/create/create_challenge_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:frontend/challenge/create/create_body_part1.dart';
 import 'package:frontend/challenge/create/create_body_part2.dart';
+import 'package:frontend/challenge/create/create_body_part3.dart';
 import 'package:frontend/model/config/palette.dart';
 import 'package:frontend/model/data/challenge.dart';
 
@@ -18,7 +19,7 @@ class _CreateChallengeState extends State<CreateChallenge> {
   bool? isPrivateSelected;
 
   bool showAdditionalWidgets =
-      false; // Added state to control the visibility of additional widgets
+  false; // Added state to control the visibility of additional widgets
 
   bool showPart1 = true;
   bool showPart2 = false;
@@ -33,64 +34,66 @@ class _CreateChallengeState extends State<CreateChallenge> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back_ios),
-            onPressed: () {},
-          ),
-          title: const Text(
-            '챌린지 생성하기',
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-              fontFamily: 'Pretendard',
-            ),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
+          onPressed: () {},
+        ),
+        title: const Text(
+          '챌린지 생성하기',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Pretendard',
           ),
         ),
-        bottomNavigationBar: Container(
-          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-          color: Colors.transparent,
-          width: double.infinity,
-          child: InkWell(
-            onTap: () {
-              setState(() {
-                if (showPart1) {
-                  showPart1 = false;
-                  showPart2 = true;
-                  print(isPrivateSelected);
-                  print("```````````${newChallenge.privateCode}");
-                  // if()
-                  // newChallenge.isPrivate
-                } else if (showPart2) {
-                  showPart2 = false;
-                  showPart3 = true;
-                }
-              });
-            },
-            child: SvgPicture.asset(
-              'assets/svgs/create_challenge_btn.svg',
-              // width: double.infinity,
-              // height: 30,
-            ),
+      ),
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+        color: Colors.transparent,
+        width: double.infinity,
+        child: InkWell(
+          onTap: () {
+            setState(() {
+              if (showPart1) {
+                showPart1 = false;
+                showPart2 = true;
+                print(isPrivateSelected);
+                print("```````````${newChallenge.privateCode}");
+              } else if (showPart2) {
+                showPart2 = false;
+                showPart3 = true;
+              }
+            });
+          },
+          child: SvgPicture.asset(
+            'assets/svgs/create_challenge_btn.svg',
+            // width: double.infinity,
+            // height: 30,
           ),
         ),
-        body: (showPart1)
-            ? BodyPart1(
-                isPrivateSelected: isPrivateSelected,
-                onPrivateButtonPressed: (bool isPrivate, String _privateCode) {
-                  setState(() {
-                    isPrivateSelected = isPrivate;
-                    newChallenge.isPrivate = isPrivate;
-                    if (isPrivate) {
-                      newChallenge.privateCode = _privateCode;
-                    }else {
-                      newChallenge.privateCode = "";
-                    }
-                  });
-                },
-              )
-            : (showPart1 == false && showPart2)
-                ? BodyPart2()
-                : SizedBox(height: 10));
+      ),
+      body: (showPart1)
+          ? BodyPart1(
+        isPrivateSelected: isPrivateSelected,
+        onPrivateButtonPressed: (bool isPrivate, String _privateCode) {
+          setState(() {
+            isPrivateSelected = isPrivate;
+            newChallenge.isPrivate = isPrivate;
+            if (isPrivate) {
+              newChallenge.privateCode = _privateCode;
+            } else {
+              newChallenge.privateCode = "";
+            }
+          });
+        },
+      )
+          : (showPart1 == false && showPart2)
+          ? BodyPart2()
+          : (showPart1 == false && showPart2 == false && showPart3)
+        ? BodyPart3()
+          :SizedBox(height: 10)
+    );
   }
+
 }
