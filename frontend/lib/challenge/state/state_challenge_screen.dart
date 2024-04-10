1e4adaf8265ea80e3d79ace7f4c94dd3860e1bcf
HyeEunShin <as221323@kookmin.ac.kr>
2024-04-10 17:00:35 +0900

    [FE/style] State_Challenge_Screen : ADD 내 인증 현황 view (#40)

--- frontend/lib/challenge/state/state_challenge_screen.dart
+++ frontend/lib/challenge/state/state_challenge_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:frontend/challenge/detail/detail_imageDetail_screen.dart';
@@ -10,6 +11,7 @@ import 'package:frontend/model/config/palette.dart';
 import 'package:frontend/model/data/challenge.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
+import 'package:simple_progress_indicators/simple_progress_indicators.dart';
 
 class ChallengeStateScreen extends StatelessWidget {
   ChallengeStateScreen({super.key});
@@ -83,8 +85,11 @@ class ChallengeStateScreen extends StatelessWidget {
               'assets/svgs/divider.svg',
               fit: BoxFit.contain,
             ),
-
-            // certificationMethod(screenWidth, screenHeight)
+            certificationState(screenWidth, screenHeight),
+            SvgPicture.asset(
+              'assets/svgs/divider.svg',
+              fit: BoxFit.contain,
+            ),
           ],
         ),
       ),
@@ -98,8 +103,6 @@ class ChallengeStateScreen extends StatelessWidget {
           onTap: () {},
           child: SvgPicture.asset(
             'assets/svgs/certification_bottom_btn.svg',
-            // width: double.infinity,
-            // height: 30,
           ),
         ),
       ),
@@ -110,7 +113,7 @@ class ChallengeStateScreen extends StatelessWidget {
     return Stack(children: <Widget>[
       Container(
         height: screenHeight * 0.3,
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage('assets/images/challenge_image.png'),
             fit: BoxFit.fitWidth,
@@ -136,7 +139,7 @@ class ChallengeStateScreen extends StatelessWidget {
 
   Widget FrequeuncyAndPeriod() {
     return Container(
-        padding: EdgeInsets.symmetric(horizontal: 5, vertical: 2),
+        padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 2),
         alignment: Alignment.center,
         decoration: BoxDecoration(
           color: Palette.purPle700,
@@ -249,8 +252,7 @@ class ChallengeStateScreen extends StatelessWidget {
                                   fontSize: 10,
                                   fontFamily: "Pretendard",
                                   fontWeight: FontWeight.w500)),
-                          Text(
-                              "${challenge.participants.length}명",
+                          Text("${challenge.participants.length}명",
                               style: const TextStyle(
                                   color: Palette.grey300,
                                   fontSize: 10,
@@ -264,7 +266,7 @@ class ChallengeStateScreen extends StatelessWidget {
   Widget ChallengeExplanation() {
     return Container(
       width: double.infinity,
-      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.start,
@@ -287,159 +289,136 @@ class ChallengeStateScreen extends StatelessWidget {
     );
   }
 
-  Widget certificationMethod(double screenWidth, double screenHeight) {
+  Widget certificationState(double screenWidth, double screenHeight) {
+    int my_certification_number = 13;
+    int entire_certification_number = 15;
+    int fail_num = 0;
+    int participant_number = 1000;
+    int participant_100 = 100;
+    int participant_80 = 300;
+    int participant_under = 600;
+
     return Container(
-      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text("인증 방식",
+          const Text("내 인증 현황",
               style: TextStyle(
                   fontSize: 15,
                   fontFamily: "Pretendard",
                   fontWeight: FontWeight.w600)),
           const SizedBox(height: 20),
-          Container(
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-              decoration: BoxDecoration(
-                  color: Color(0xFFF5F5F5), // 배경색 설정
-                  borderRadius: BorderRadius.circular(10)), // 컨테이너를 둥글게 만듭니다.
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text("인증 가능 시간",
-                            style: TextStyle(
-                                color: Palette.grey200,
-                                fontSize: 10,
-                                fontFamily: "Pretendard",
-                                fontWeight: FontWeight.w500)),
-                        Text(
-                            "${challenge.certificationStartTime}시 - ${challenge.certificationEndTime}",
-                            style: const TextStyle(
-                                color: Palette.grey300,
-                                fontSize: 10,
-                                fontFamily: "Pretendard",
-                                fontWeight: FontWeight.w700))
-                      ],
-                    ),
-                    const SizedBox(height: 3),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text("인증 횟수",
-                            style: TextStyle(
-                                color: Palette.grey200,
-                                fontSize: 10,
-                                fontFamily: "Pretendard",
-                                fontWeight: FontWeight.w500)),
-                        Text(challenge.certificationFrequency,
-                            style: const TextStyle(
-                                color: Palette.grey300,
-                                fontSize: 10,
-                                fontFamily: "Pretendard",
-                                fontWeight: FontWeight.w700))
-                      ],
-                    ),
-                    const SizedBox(height: 3),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text("수단",
-                            style: TextStyle(
-                                color: Palette.grey200,
-                                fontSize: 10,
-                                fontFamily: "Pretendard",
-                                fontWeight: FontWeight.w500)),
-                        Text(challenge.isGalleryPossible ? "카메라+갤러리" : "카메라",
-                            style: const TextStyle(
-                                color: Palette.grey300,
-                                fontSize: 10,
-                                fontFamily: "Pretendard",
-                                fontWeight: FontWeight.w700))
-                      ],
-                    )
-                  ])),
-          const SizedBox(height: 10),
-          Container(
-              padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-              child: Text(challenge.certificationExplanation,
-                  style: const TextStyle(
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Row(children: [
+              Text("${my_certification_number}회",
+                  style: TextStyle(
                       fontSize: 10,
+                      color: Palette.purPle400,
                       fontFamily: "Pretendard",
-                      fontWeight: FontWeight.w500))),
+                      fontWeight: FontWeight.w600)),
+              Text(
+                  " / ${entire_certification_number}회  |  남은 인증 : ${entire_certification_number - my_certification_number}회",
+                  style: TextStyle(
+                      fontSize: 10,
+                      color: Palette.grey300,
+                      fontFamily: "Pretendard",
+                      fontWeight: FontWeight.w600))
+            ]),
+            Text("실패 횟수 : ${fail_num}회",
+                style: TextStyle(
+                    fontSize: 9,
+                    color: Palette.grey300,
+                    fontFamily: "Pretendard",
+                    fontWeight: FontWeight.w400))
+          ]),
           const SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              buildImageContainer(challenge.successfulVerificationImage,
-                  Palette.green, true, screenWidth, screenHeight),
-              SizedBox(width: 10),
-              buildImageContainer(challenge.failedVerificationImage,
-                  Palette.red, false, screenWidth, screenHeight),
-            ],
+          certificationStateBar(screenWidth),
+          const SizedBox(height: 10),
+          Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+            width: screenWidth,
+            color: Colors.transparent,
+            child: InkWell(
+              onTap: () {},
+              child: SvgPicture.asset(
+                'assets/svgs/go_Certification_btn.svg',
+              ),
+            ),
           ),
         ],
       ),
     );
   }
 
-  Widget buildImageContainer(File? file, Color color, bool isSuccess,
-      double screenWidth, double screenHeight) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SvgPicture.asset(
-              isSuccess
-                  ? 'assets/svgs/check_green.svg'
-                  : 'assets/svgs/check_red.svg',
-            ),
-            const SizedBox(width: 3),
-            Text(
-              isSuccess ? "성공 예시" : "실패 예시",
-              style: const TextStyle(
-                  fontFamily: 'Pretendard',
-                  fontSize: 10,
-                  fontWeight: FontWeight.w500,
-                  color: Palette.grey500),
-            ),
-          ],
-        ),
-        const SizedBox(height: 5),
-        Container(
-          decoration: BoxDecoration(
-            color: Palette.greySoft,
-            borderRadius: BorderRadius.circular(33.0),
-            border: Border.all(
-              color: file != null ? color : Palette.greySoft,
-              width: 2.0,
-            ),
+  Widget certificationStateBar(double screenWidth) {
+    int my_certification_number = 13;
+    int entire_certification_number = 15;
+    String percent =
+        (my_certification_number / entire_certification_number * 100)
+            .toStringAsFixed(1);
+
+    print(percent);
+    int fail_num = 0;
+    int participant_number = 1000;
+    int participant_100 = 100;
+    int participant_80 = 300;
+    int participant_under = 600;
+
+    return Container(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        decoration: BoxDecoration(
+            color: Color(0xFFF5F5F5), // 배경색 설정
+            borderRadius: BorderRadius.circular(10)), // 컨테이너를 둥글게 만듭니다.
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center, children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text("현재 달성률",
+                  style: TextStyle(
+                      color: Palette.grey500,
+                      fontSize: 10,
+                      fontFamily: "Pretendard",
+                      fontWeight: FontWeight.w500)),
+              Text(
+                  "${(my_certification_number / entire_certification_number * 100).toStringAsFixed(1)} %",
+                  style: const TextStyle(
+                      color: Palette.grey500,
+                      fontSize: 10,
+                      fontFamily: "Pretendard",
+                      fontWeight: FontWeight.w700))
+            ],
           ),
-          height: screenHeight * 0.16,
-          width: screenWidth * 0.42,
-          child: file != null
-              ? ClipRRect(
-                  borderRadius: BorderRadius.circular(30.0),
-                  child: Image.file(
-                    File(file.path),
-                    fit: BoxFit.cover,
-                  ),
-                )
-              : const Icon(
-                  Icons.image,
-                  size: 35,
-                  color: Palette.grey300,
-                ),
-        ),
-        const SizedBox(height: 5),
-      ],
-    );
+          const SizedBox(height: 5),
+          Container(
+            child: ProgressBar(
+              width: screenWidth * 0.75,
+              height: 4,
+              value: double.parse(percent) * 0.01,
+              backgroundColor: Palette.grey50,
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Palette.purPle200, Palette.mainPurple],
+              ),
+            ),
+            decoration: BoxDecoration(
+                color: Palette.grey500, // 배경색 설정
+                borderRadius: BorderRadius.circular(5)),
+          ),
+          const SizedBox(height: 5),
+          const Text("예상 달성률 : 100%",
+
+              style: TextStyle(
+
+                  color: Palette.grey200,
+                  fontSize: 10,
+                  fontFamily: "Pretendard",
+                  fontWeight: FontWeight.w500)),
+        ]));
   }
 }
--- frontend/lib/model/config/palette.dart
+++ frontend/lib/model/config/palette.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/painting.dart';
 
 class Palette {
-  //9754FB
+  //Main
   static const Color mainPurple = Color(0xFF5924ED);
+  //Grey
   static const Color white = Color(0xFFFFFFFF);
+  static const Color greySoft = Color(0xFFF5F5F5);
+  static const Color grey50 = Color(0xFFE8E8E9);
   static const Color grey200 = Color(0xFF96979B);
   static const Color grey300 = Color(0xFF66676D);
   static const Color grey500 = Color(0xFF1A1C25);
-  static const Color greySoft = Color(0xFFF5F5F5);
+  //Purple
   static const Color softPurPle = Color(0xFFEEE9FD);
+  static const Color purPle50 = Color(0xFFEEE9FD);
   static const Color purPle200 = Color(0xFFB39AF7);
   static const Color purPle400 = Color(0xFF7A50F1);
   static const Color purPle700 = Color(0xFF3F1AA8);
+  //etc
   static const Color green = Color(0xFF1CC036);
   static const Color red = Color(0xFFD72D2D);
-
-
 }
