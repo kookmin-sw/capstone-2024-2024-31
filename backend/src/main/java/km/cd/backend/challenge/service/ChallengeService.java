5e5208ea45abf380ef9636b69bbbb115b315fe8a
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-04-12 15:11:38 +0900

    [backend/feat] 챌린지 생성 시 사진 저장 기능 구현

--- backend/src/main/java/km/cd/backend/challenge/service/ChallengeService.java
+++ backend/src/main/java/km/cd/backend/challenge/service/ChallengeService.java
@@ -5,6 +5,7 @@ import km.cd.backend.challenge.domain.Participant;
 import km.cd.backend.challenge.dto.ChallengeReceivedDto;
 import km.cd.backend.challenge.dto.ChallengeResponseDto;
 import km.cd.backend.challenge.repository.ChallengeRepository;
+import km.cd.backend.common.utils.S3Uploader;
 import km.cd.backend.user.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,15 +14,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class ChallengeService {
     private final ChallengeRepository challengeRepository;
 
-    public ChallengeService(ChallengeRepository challengeRepository) {
+    private final S3Uploader s3Uploader;
+    public ChallengeService(ChallengeRepository challengeRepository, S3Uploader s3Uploader) {
         this.challengeRepository = challengeRepository;
+        this.s3Uploader = s3Uploader;
     }
 
     @Transactional
     public Challenge createChallenge(ChallengeReceivedDto challengeReceivedDTO, User user) {
         // 프론트로부터 넘겨받은 챌린지 데이터
-        Challenge challenge = challengeReceivedDTO.toEntity();
-
+        Challenge challenge = challengeReceivedDTO.toEntity(s3Uploader);
+        
+        System.out.println(challenge);
         // participant 생성
         Participant creator = new Participant();
         creator.setChallenge(challenge);
