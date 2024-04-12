7c21901f1cc60783942ef457905968b099deaa2c
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-04-12 15:11:17 +0900

    [backend/fix] responseDto lombok 적용

--- backend/src/main/java/km/cd/backend/challenge/dto/ChallengeResponseDto.java
+++ backend/src/main/java/km/cd/backend/challenge/dto/ChallengeResponseDto.java
@@ -2,63 +2,26 @@ package km.cd.backend.challenge.dto;
 
 import km.cd.backend.challenge.domain.Challenge;
 import km.cd.backend.challenge.domain.Participant;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.Date;
 import java.util.List;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChallengeResponseDto {
-    private Long challengeId;
-    private String challengeName;
-    private Date startDate;
-    private Date endDate;
-    private Integer certificationFrequency;
-    private String certificationExplanation;
-    private Integer certificationCount;
-    private String certificationMethod;
-    private String challengeExplanation;
-    private Integer maximumPeople;
-    private Boolean isPrivate;
-    private String privateCode;
+    private Long challenge_id;
+    private String challenge_name;
+    private Date start_date;
+    private Date end_date;
+    private String certification_frequency;
+    private String certification_explanation;
+    private Integer maximum_people;
+    private Boolean is_private;
+    private String private_code;
     private List<Participant> participants;
-
-    public ChallengeResponseDto() {
-    }
-
-    public ChallengeResponseDto(Long challengeId, String challengeName, Date startDate, Date endDate, Integer certificationFrequency,
-        String certificationExplanation, Integer certificationCount, String certificationMethod,
-        String challengeExplanation, Integer maximumPeople, Boolean isPrivate, String privateCode, List<Participant> participants) {
-        this.challengeId = challengeId;
-        this.challengeName = challengeName;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.certificationFrequency = certificationFrequency;
-        this.certificationExplanation = certificationExplanation;
-        this.certificationCount = certificationCount;
-        this.certificationMethod = certificationMethod;
-        this.challengeExplanation = challengeExplanation;
-        this.maximumPeople = maximumPeople;
-        this.isPrivate = isPrivate;
-        this.privateCode = privateCode;
-        this.participants = participants;
-    }
-
-    public static ChallengeResponseDto toDto(Challenge challenge) {
-        return new ChallengeResponseDto(
-            challenge.getChallenge_id(),
-            challenge.getChallenge_name(),
-            challenge.getStart_date(),
-            challenge.getEnd_date(),
-            challenge.getCertification_frequency(),
-            challenge.getCertification_explanation(),
-            challenge.getCertification_count(),
-            challenge.getCertification_method(),
-            challenge.getChallenge_explanation(),
-            challenge.getMaximum_people(),
-            challenge.getIs_private(),
-            challenge.getPrivate_code(),
-            challenge.getParticipants()
-        );
-    }
 }
+
