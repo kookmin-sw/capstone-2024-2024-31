5027a9db53330b0e68559d224e3f402b10adb697
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-04-19 15:59:38 +0900

    [backend/feat] 인증 dto 생성

---
+++ backend/src/main/java/km/cd/backend/challenge/dto/CertificationReceivedDto.java
@@ -0,0 +1,19 @@
+package km.cd.backend.challenge.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CertificationReceivedDto {
+    
+    @Schema(description = "챌린지 아이디")
+    private Long challengeId;
+    
+    @Schema(description = "챌린지 인증 이미지")
+    private MultipartFile certificationImage;
+}
