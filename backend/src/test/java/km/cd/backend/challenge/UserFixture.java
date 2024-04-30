23a82abbad0fb0339c6fa23817ac168b1e116611
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-04-30 16:49:30 +0900

    [backend/feat] test fixture 추가

---
+++ backend/src/test/java/km/cd/backend/challenge/UserFixture.java
@@ -0,0 +1,24 @@
+package km.cd.backend.challenge;
+
+
+import km.cd.backend.user.User;
+
+public class UserFixture {
+    
+    public static User user() {
+        return User.builder()
+            .id(1L)
+            .email("example@example.com")
+            .password("password123")
+            .provider("local")
+            .providerId("local123")
+            .name("John Doe")
+            .avatar("avatar.jpg")
+            .role("ROLE_USER")
+            .level(1)
+            .xp(0)
+            .point(0)
+            .build();
+    }
+    
+}
