bfb87612c60d61c57572d10afac36610576ff7d5
HyeEunShin <as221323@kookmin.ac.kr>
2024-05-01 15:44:37 +0900

    Create certification_screen.dart

---
+++ frontend/lib/challenge/certification/certification_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CertificationScreen extends StatefulWidget {
+  const CertificationScreen({super.key});
+
+  @override
+  State<CertificationScreen> createState() => _CertificationScreenState();
+}
+
+class _CertificationScreenState extends State<CertificationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
