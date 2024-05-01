577b4d55b5fc626addc1f706e487832d8d6524e7
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-05-01 20:57:57 +0900

    [backend/style] 괄호 제거

--- backend/src/main/java/km/cd/backend/challenge/domain/Challenge.java
+++ backend/src/main/java/km/cd/backend/challenge/domain/Challenge.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Getter @Setter
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor()
+@NoArgsConstructor
 public class Challenge {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
