83dd52cc7b4f0b45d3ecdf8225fc8d92dde2aa0a
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-05-03 00:06:54 +0900

    [backend/fix] 챌린지 응답 필드 추가

--- backend/src/main/java/km/cd/backend/challenge/dto/response/ChallengeInformationResponse.java
+++ backend/src/main/java/km/cd/backend/challenge/dto/response/ChallengeInformationResponse.java
@@ -1,28 +1,50 @@
 package km.cd.backend.challenge.dto.response;
 
-import km.cd.backend.certification.domain.CertificationType;
-import km.cd.backend.challenge.domain.Participant;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.Date;
 import java.util.List;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class ChallengeInformationResponse {
     private Long id;
-    private String challengeName;
-    private Date startDate;
-    private Date endDate;
-    private String certificationFrequency;
-    private String certificationExplanation;
-    private CertificationType certificationType;
-    private Integer maximumPeople;
+    
     private Boolean isPrivate;
+    
     private String privateCode;
-    private List<Participant> participants;
+    
+    private String challengeName;
+    
+    private String challengeExplanation;
+    
+    private Integer challengePeriod;
+    
+    private Date startDate;
+    
+    private Date endDate;
+    
+    private String certificationFrequency;
+    
+    private Integer certificationStartTime;
+    
+    private Integer certificationEndTime;
+    
+    private String certificationExplanation;
+    
+    private Boolean isGalleryPossible;
+    
+    private Integer maximumPeople;
+    
+    private List<String> challengeImagePaths;
+    
+    private String failedVerificationImage;
+    
+    private String successfulVerificationImage;
+    
+    private Boolean isEnded;
+    
+    private Integer totalParticipants ;
 }
 
