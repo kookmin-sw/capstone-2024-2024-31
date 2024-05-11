111b8ff7f26b0a4739c3ed4893367240ab9a50cb
HyeEunShin <as221323@kookmin.ac.kr>
2024-05-11 15:18:19 +0900

    [FE/refactor]: 커뮤니티페이지에서 CHAT 탭 뷰 제거

--- frontend/lib/community/tab_community_screen.dart
+++ frontend/lib/community/tab_community_screen.dart
@@ -42,57 +42,8 @@ class _TabCommunityScreenState extends State<TabCommunityScreen>
                     fontWeight: FontWeight.bold,
                     fontSize: 16),
               )),
-          bottom: PreferredSize(
-            preferredSize: const Size.fromHeight(40),
-            child: ClipRRect(
-                borderRadius: const BorderRadius.all(Radius.circular(10)),
-                child: Container(
-                    height: 40,
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: TabBar(
-                      indicatorSize: TabBarIndicatorSize.label,
-                      dividerHeight: 0,
-                      padding: EdgeInsets.only(right: size.width * 0.4),
-                      controller: _tabController,
-                      splashBorderRadius: BorderRadius.circular(25),
-                      overlayColor: MaterialStateProperty.all(Palette.purPle50),
-                      tabs: const [
-                        Tab(
-                          child: Text(
-                            '커뮤니티',
-                            style: TextStyle(
-                                fontSize: 13, fontFamily: 'Pretendard'),
-                          ),
-
-                        ),
-                        Tab(
-                          child: Text(
-                            'CHAT',
-                            style: TextStyle(
-                                fontSize: 13, fontFamily: 'Pretendard'),
-                          ),
-                        ),
-                      ],
-                      indicator: const BoxDecoration(
-                          border: Border(
-                              bottom: BorderSide(
-                                  color: Palette.mainPurple, width: 2.0))),
-                      labelColor: Palette.purPle700,
-                      labelStyle: const TextStyle(fontWeight: FontWeight.bold),
-
-                      unselectedLabelStyle:
-                          const TextStyle(fontWeight: FontWeight.normal),
-                    ))),
-          )),
-      body: TabBarView(
-          controller: _tabController,
-          physics: const NeverScrollableScrollPhysics(),
-          children: [const CommunityScreen(), ChattingList()]),
+      ),
+      body: const CommunityScreen()
     );
-  }
-
-
-  Widget ChattingList() {
-    return Container();
   }
 }
