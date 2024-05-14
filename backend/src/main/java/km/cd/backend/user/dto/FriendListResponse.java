b8a3e0fcb53bf0ad07d9c71d54d0c74688169057
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-05-15 00:23:55 +0900

    [backend/fix] 필드 제거

--- backend/src/main/java/km/cd/backend/user/dto/FriendListResponse.java
+++ backend/src/main/java/km/cd/backend/user/dto/FriendListResponse.java
@@ -1,14 +1,12 @@
 package km.cd.backend.user.dto;
 
-import km.cd.backend.user.domain.FriendStatus;
 import lombok.Builder;
 import lombok.Data;
 
 @Builder
 @Data
 public class FriendListResponse {
-    private Long friendShipId;
+    private Long friendId;
     private String friendEmail;
     private String friendName;
-    private FriendStatus status;
 }
