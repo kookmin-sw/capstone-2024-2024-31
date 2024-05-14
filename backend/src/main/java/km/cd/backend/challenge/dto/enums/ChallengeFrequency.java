7a72a5e4431e32bd93458b5293fd5ade174a832e
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-05-14 16:12:53 +0900

    [backend/fix] 챌린지 주기 설정 변경

--- backend/src/main/java/km/cd/backend/challenge/dto/enums/ChallengeFrequency.java
+++ backend/src/main/java/km/cd/backend/challenge/dto/enums/ChallengeFrequency.java
@@ -4,19 +4,19 @@ import lombok.Getter;
 
 @Getter
 public enum ChallengeFrequency {
-  EVERY_WEEKDAY("평일매일", 7),
-  EVERY_WEEKEND("주말매일", 2),
-  WEEKDAYS("평일",5),
-  EVERY_WEEK("주1일",1),
-  EVERY_2ND_WEEK("주2일",2),
-  EVERY_3RD_WEEK("주3일",3),
-  EVERY_4TH_WEEK("주4일",4),
-  EVERY_5TH_WEEK("주5일",5),
-  EVERY_6TH_WEEK("주6일",6);
-
+  EVERY_WEEKDAY("평일 매일", 5),
+  EVERY_WEEKEND("주말 매일", 2),
+  EVERY_DAY("매일",7),
+  EVERY_WEEK("주 1회",1),
+  EVERY_2ND_WEEK("주 2회",2),
+  EVERY_3RD_WEEK("주 3회",3),
+  EVERY_4TH_WEEK("주 4회",4),
+  EVERY_5TH_WEEK("주 5회",5),
+  EVERY_6TH_WEEK("주 6회",6);
+  
   private final String frequency;
   private final int daysPerWeek;
-
+  
   ChallengeFrequency(String frequency, int daysPerWeek) {
     this.frequency = frequency;
     this.daysPerWeek = daysPerWeek;
