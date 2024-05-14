4791bb2e7364a4c894cfe9b027829b37d15554be
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-05-14 12:30:54 +0900

    [backend/feat] friend 엔티티 추가

---
+++ backend/src/main/java/km/cd/backend/user/domain/Friend.java
@@ -0,0 +1,41 @@
+package km.cd.backend.user.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Friend {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User users;
+    
+    private String userEmail;
+    private String friendEmail;
+    private FriendStatus status;
+    private Long counterpartId;
+    private boolean isFrom;
+    
+    public void acceptFriendRequest() {
+        status = FriendStatus.ACCEPT;
+    }
+}
