259de140003cb9d2adc48e5ce5d1d82f232036c2
HyeEunShin <as221323@kookmin.ac.kr>
2024-05-15 21:45:38 +0900

    [FE/feat] 데이터 모델 추가

--- frontend/lib/model/data/user.dart
+++ frontend/lib/model/data/user.dart
@@ -1,37 +1,65 @@
-import 'package:frontend/model/data/challenge/challenge_simple.dart';
+import 'package:frontend/model/data/freind.dart';
 
 class User {
-  int id;
-  String email;
-  String avatar;
-  String name;
-  int point;
+  final int id;
+  final String email;
+  final String? password;
+  final String provider;
+  final String providerId;
+  final String name;
+  final String? avatar;
+  final String role;
+  final DateTime createdDate;
+  final int point;
+  final List<Friend> following;
+  final List<Friend> followers;
 
   User({
     required this.id,
     required this.email,
-    required this.avatar,
+    this.password,
+    required this.provider,
+    required this.providerId,
     required this.name,
-    required this.point,
+    this.avatar,
+    this.role = 'ROLE_USER',
+    required this.createdDate,
+    this.point = 0,
+    required this.following,
+    required this.followers,
   });
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
       id: json['id'],
       email: json['email'],
-      avatar: json['avatar'],
+      password: json['password'],
+      provider: json['provider'],
+      providerId: json['providerId'],
       name: json['name'],
-      point: json['point'],
+      avatar: json['avatar'] ,
+      role: json['role'] ?? 'ROLE_USER',
+      createdDate: DateTime.parse(json['createdDate']),
+      point: json['point'] ?? 0,
+      following: List<Friend>.from(json['following'].map((model) => Friend.fromJson(model))),
+      followers: List<Friend>.from(json['followers'].map((model) => Friend.fromJson(model))),
     );
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['id'] = id;
-    data['email'] = email;
-    data['avatar'] = avatar;
-    data['name'] = name;
-    data['point'] = point;
-    return data;
+    return {
+      'id': id,
+      'email': email,
+      'password': password,
+      'provider': provider,
+      'providerId': providerId,
+      'name': name,
+      'avatar': avatar,
+      'role': role,
+      'createdDate': createdDate.toIso8601String(),
+      'point': point,
+      'following': following.map((e) => e.toJson()).toList(),
+      'followers': followers.map((e) => e.toJson()).toList(),
+    };
   }
 }
