e79486af053ab20f0fd1e0d73ba7ed99fb0f4de2
HyeEunShin <as221323@kookmin.ac.kr>
2024-05-15 20:34:27 +0900

    [AI/feat] camera on Device

--- frontend/lib/challenge/certification/scan_controller.dart
+++ frontend/lib/challenge/certification/scan_controller.dart
@@ -57,36 +57,16 @@ class ScanController extends GetxController {
     super.onInit();
   }
 
+
   void capture() {
     if (_cameraImage != null) {
-      // Uint8List 인스턴스를 가정
-      Uint8List uint8List = _cameraImage!.planes[0].bytes;
-
-// Uint8List에서 ByteBuffer 접근
-      ByteBuffer buffer = uint8List.buffer;
-
-// 오프셋과 길이 처리
-      int offset = uint8List.offsetInBytes;
-      int length = uint8List.lengthInBytes;
-
-// ByteBuffer에서 정확한 Uint8List 부분을 추출하여 함수에 전달
-      ByteBuffer correctBuffer = buffer.asUint8List(offset, length).buffer;
-
-      // Create an img.Image from the ByteBuffer
       img.Image image = img.Image.fromBytes(
-          width: _cameraImage!.width,
-          height: _cameraImage!.height,
-          bytes: buffer.uint8List(offset, length),
-          format: img.Format.bgra  // Confirm that the format matches your camera data
-      );
-
-      // Encode the image to JPEG format and add it to the image list
-      Uint8List jpg = Uint8List.fromList(img.encodeJpg(image));
-      _imageList.add(jpg);
+          _cameraImage!.width, _cameraImage!.height,
+          _cameraImage!.planes[0].bytes, format: img.Format.bgra);
+      Uint8List list = Uint8List.fromList(img.encodeJpg(image));
+      _imageList.add(list);
       _imageList.refresh();
     }
   }
-
-
 }
 
