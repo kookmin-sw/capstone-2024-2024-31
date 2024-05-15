0ba8dfa4476c1391097b9986a1a0fafa9f6c589f
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-05-15 19:47:52 +0900

    [backend/feat] 유저 이미지 crud 기능 구현

--- backend/src/main/java/km/cd/backend/user/UserService.java
+++ backend/src/main/java/km/cd/backend/user/UserService.java
@@ -3,14 +3,18 @@ package km.cd.backend.user;
 import km.cd.backend.challenge.domain.Challenge;
 import km.cd.backend.challenge.domain.Participant;
 import km.cd.backend.challenge.domain.mapper.ChallengeMapper;
+import km.cd.backend.challenge.dto.enums.FilePathEnum;
 import km.cd.backend.challenge.dto.response.ChallengeSimpleResponse;
 import km.cd.backend.challenge.repository.ParticipantRepository;
 import km.cd.backend.common.error.CustomException;
 import km.cd.backend.common.error.ExceptionCode;
+import km.cd.backend.common.utils.s3.S3Uploader;
 import km.cd.backend.user.domain.Friend;
 import km.cd.backend.user.domain.User;
 import km.cd.backend.user.domain.mapper.FriendMapper;
+import km.cd.backend.user.domain.mapper.UserMapper;
 import km.cd.backend.user.dto.FriendListResponse;
+import km.cd.backend.user.dto.UserResponse;
 import km.cd.backend.user.repository.FriendRepository;
 import km.cd.backend.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +22,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @Transactional
@@ -27,6 +32,7 @@ public class UserService {
     private final UserRepository userRepository;
     private final FriendRepository friendRepository;
     private final ParticipantRepository participantRepository;
+    private final S3Uploader s3Uploader;
 
     public User findByEmail(String email) {
         return userRepository.findByEmail(email).orElseThrow(() -> new CustomException(ExceptionCode.USER_NOT_FOUND));
@@ -86,4 +92,38 @@ public class UserService {
                 }
         ).toList();
     }
+    
+    public UserResponse uploadProfileImage(MultipartFile profileImage, Long userId) {
+        User user = findById(userId);
+        String profileImageUrl = s3Uploader.uploadFileToS3(profileImage, FilePathEnum.USER.getPath());
+        user.setProfileImage(profileImageUrl);
+        userRepository.save(user);
+        
+        return UserMapper.INSTANCE.userToUserResponse(user);
+    }
+    
+    public UserResponse updateProfileImage(MultipartFile profileImage, Long userId){
+        User user = findById(userId);
+        s3Uploader.deleteS3(user.getProfileImage());
+        
+        String profileImageUrl = s3Uploader.uploadFileToS3(profileImage, FilePathEnum.USER.getPath());
+        user.setProfileImage(profileImageUrl);
+        userRepository.save(user);
+        
+        return UserMapper.INSTANCE.userToUserResponse(user);
+    }
+    
+    public UserResponse deleteProfileImage(Long userId) {
+        User user = findById(userId);
+        s3Uploader.deleteS3(user.getProfileImage());
+        user.setProfileImage(null);
+        userRepository.save(user);
+        
+        return UserMapper.INSTANCE.userToUserResponse(user);
+    }
+    
+    public String getProfileImage(Long userId) {
+        return findById(userId).getProfileImage();
+    }
+    
 }
