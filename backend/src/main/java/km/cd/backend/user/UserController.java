0b8660899703957f2efe52e0f1d492b7533f445a
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-05-15 19:47:39 +0900

    [backend/feat] 유저 이미지 crud 기능 구현

--- backend/src/main/java/km/cd/backend/user/UserController.java
+++ backend/src/main/java/km/cd/backend/user/UserController.java
@@ -8,16 +8,20 @@ import km.cd.backend.user.dto.UserResponse;
 import km.cd.backend.user.dto.FriendListResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/users")
@@ -68,7 +72,6 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.ACCEPTED).build();
     }
     
-
     @GetMapping("/me/challenges")
     public ResponseEntity<List<ChallengeSimpleResponse>> myChallenges(
             @AuthenticationPrincipal PrincipalDetails principalDetails
@@ -77,5 +80,34 @@ public class UserController {
         List<ChallengeSimpleResponse> challengeSimpleResponses = userService.getChallengesByUserId(userId);
         return ResponseEntity.ok(challengeSimpleResponses);
     }
-
+    
+    @PostMapping(path = "/image", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<UserResponse> uploadProfileImage(
+        @RequestPart(name = "profileImage") MultipartFile profileImage,
+        @AuthenticationPrincipal PrincipalDetails principalDetails
+    ) {
+        return ResponseEntity.ok(userService.uploadProfileImage(profileImage, principalDetails.getUserId()));
+    }
+    
+    @PutMapping(path = "/image", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<UserResponse> updateProfileImage(
+        @RequestPart(name = "profileImage") MultipartFile profileImage,
+        @AuthenticationPrincipal PrincipalDetails principalDetails
+    ) {
+        return ResponseEntity.ok(userService.updateProfileImage(profileImage, principalDetails.getUserId()));
+    }
+    
+    @DeleteMapping("/image")
+    public ResponseEntity<UserResponse> deleteProfileImage(
+        @AuthenticationPrincipal PrincipalDetails principalDetails
+    ) {
+        return ResponseEntity.ok(userService.deleteProfileImage(principalDetails.getUserId()));
+    }
+    
+    @GetMapping("/image")
+    public ResponseEntity<String> getProfileImage(
+        @AuthenticationPrincipal PrincipalDetails principalDetails
+    ) {
+        return ResponseEntity.ok(userService.getProfileImage(principalDetails.getUserId()));
+    }
 }
