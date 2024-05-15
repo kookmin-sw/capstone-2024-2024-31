0a61a3daeef858b5c76c510057219fa880d2275c
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-05-15 20:19:24 +0900

    [backend/fix] mapper 변경

--- backend/src/main/java/km/cd/backend/user/domain/mapper/FriendMapper.java
+++ backend/src/main/java/km/cd/backend/user/domain/mapper/FriendMapper.java
@@ -1,14 +1,9 @@
 package km.cd.backend.user.domain.mapper;
 
-import java.util.ArrayList;
-import java.util.List;
 import km.cd.backend.user.domain.Friend;
-import km.cd.backend.user.domain.User;
 import km.cd.backend.user.dto.FriendListResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
-import org.mapstruct.control.MappingControl.Use;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -18,15 +13,7 @@ public interface FriendMapper {
     
     @Mapping(source = "friend.id", target = "friendId")
     @Mapping(target = "friendEmail", expression = "java(isFromUser ? friend.getFriendEmail() : friend.getMyEmail())")
-    FriendListResponse FRIEND_LIST_RESPONSE(Friend friend, boolean isFromUser);
-    
-    @Named("mapFriendListResponse")
-    default ArrayList<FriendListResponse> FRIEND_LIST_RESPONSE_LIST(List<Friend> friendList, boolean isFromUser) {
-        ArrayList<FriendListResponse> resultList = new ArrayList<>();
-        for (Friend friend : friendList) {
-            resultList.add(FRIEND_LIST_RESPONSE(friend, isFromUser));
-        }
-        return resultList;
-    }
+    @Mapping(source = "friendImage", target = "friendImage")
+    FriendListResponse FRIEND_LIST_RESPONSE(Friend friend, boolean isFromUser, String friendImage);
 }
 
