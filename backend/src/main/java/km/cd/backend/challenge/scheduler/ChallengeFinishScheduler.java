428339c8a709f2c41daa417f657f0aa169e6f786
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-05-16 12:21:27 +0900

    [backend/fix] 챌린지 시작 스케쥴러 등록

--- backend/src/main/java/km/cd/backend/challenge/scheduler/ChallengeFinishScheduler.java
+++ backend/src/main/java/km/cd/backend/challenge/scheduler/ChallengeFinishScheduler.java
@@ -1,8 +1,11 @@
 package km.cd.backend.challenge.scheduler;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 import km.cd.backend.challenge.domain.Challenge;
+import km.cd.backend.challenge.repository.ChallengeRepository;
 import km.cd.backend.challenge.service.ChallengeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -15,13 +18,21 @@ import org.springframework.stereotype.Component;
 public class ChallengeFinishScheduler {
 
     private final ChallengeService challengeService;
+    private final ChallengeRepository challengeRepository;
     
     @Scheduled(cron = "0 0 0 * * ?") // 초, 분, 시 일, 월, 주(요일)
     public void checkChallengeEndDate() {
-        Date currentDate = new Date();
-        List<Challenge> challengeToEnd = challengeService.findChallengesByEndDate(currentDate);
+        List<Challenge> challengeToEnd = challengeRepository.findByStartDate(LocalDate.now());
         for (Challenge challenge : challengeToEnd) {
             challengeService.finishChallenge(challenge);
+        }
+    }
+    
+    @Scheduled(cron = "0 0 0 * * ?") // 초, 분, 시 일, 월, 주(요일)
+    public void checkChallengeStartDate() {
+        List<Challenge> challengeToEnd = challengeRepository.findByStartDate(LocalDate.now());
+        for (Challenge challenge : challengeToEnd) {
+            challengeService.startChallenge(challenge);
         }
     }
 }
