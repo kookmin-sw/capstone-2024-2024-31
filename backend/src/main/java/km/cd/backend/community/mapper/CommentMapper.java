7960eab4cd77db8534067ccff2f6fef70a482534
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-05-16 10:32:57 +0900

    [backend/feat] list mapping 추가

--- backend/src/main/java/km/cd/backend/community/mapper/CommentMapper.java
+++ backend/src/main/java/km/cd/backend/community/mapper/CommentMapper.java
@@ -1,7 +1,11 @@
 package km.cd.backend.community.mapper;
 
+import java.util.List;
 import km.cd.backend.community.domain.Comment;
 import km.cd.backend.community.dto.CommentResponse;
+import km.cd.backend.user.domain.User;
+import km.cd.backend.user.domain.mapper.UserMapper;
+import km.cd.backend.user.dto.UserResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
@@ -12,5 +16,11 @@ public interface CommentMapper {
     CommentMapper INSTANCE = Mappers.getMapper(CommentMapper.class);
 
     @Mapping(target = "author", source = "author.name")
+    @Mapping(target = "userResponse", source = "author")
     CommentResponse entityToResponse(Comment comment);
+    
+    List<CommentResponse> COMMENT_RESPONSE_LIST(List<Comment> comments);
+    default UserResponse USER_RESPONSE(User user) {
+        return UserMapper.INSTANCE.userToUserResponse(user);
+    }
 }
