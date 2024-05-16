f3c1830023a0f5f5f4c0465816ed74bf514bf40a
HyeEunShin <as221323@kookmin.ac.kr>
2024-05-16 16:55:31 +0900

    [all] 챌린지 조회 에러..

--- frontend/lib/model/data/challenge/challenge_category.dart
+++ frontend/lib/model/data/challenge/challenge_category.dart
@@ -7,4 +7,14 @@ enum ChallengeCategory {
 
   final String name;
   const ChallengeCategory(this.name);
+
+  // JSON에서 객체로 변환
+  factory ChallengeCategory.fromJson(String name) {
+    return ChallengeCategory.values.firstWhere((e) => e.name == name);
+  }
+
+  // 객체에서 JSON으로 변환
+  String toJson() {
+    return name;
+  }
 }
