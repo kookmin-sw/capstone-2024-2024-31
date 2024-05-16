d5e748dc51520d5fa6b38044b75e7f7565ae73ac
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-05-16 19:27:44 +0900

    [backend/feat] github 커밋 확인 추가

--- backend/src/main/java/km/cd/backend/certification/controller/CertificationController.java
+++ backend/src/main/java/km/cd/backend/certification/controller/CertificationController.java
@@ -1,9 +1,13 @@
 package km.cd.backend.certification.controller;
 
+import km.cd.backend.certification.dto.GithubCommitResponse;
 import km.cd.backend.certification.service.CertificationService;
+import km.cd.backend.common.jwt.PrincipalDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,6 +22,13 @@ public class CertificationController {
     public ResponseEntity<Void> certifyHandGesture(@RequestPart(name = "image") MultipartFile image) {
         certificationService.certifyHandGesture(image);
         return ResponseEntity.ok(null);
+    }
+
+    @PostMapping(value = "/github-commit")
+    public ResponseEntity<GithubCommitResponse> certifyGitubCommit(
+        @AuthenticationPrincipal PrincipalDetails principalDetails
+    ) {
+        return ResponseEntity.ok(certificationService.certifyGitubCommit(principalDetails.getUserId()));
     }
 
 }
