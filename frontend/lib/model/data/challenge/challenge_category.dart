8e4db6affe21ce696db8b976d2e5ea9a039d3863
HyeEunShin <as221323@kookmin.ac.kr>
2024-05-17 16:21:09 +0900

    [all/feat] 유저 카테고리 연동 후 반영까지 됨

--- frontend/lib/model/data/challenge/challenge_category.dart
+++ frontend/lib/model/data/challenge/challenge_category.dart
@@ -1,3 +1,5 @@
+import 'package:logger/logger.dart';
+
 enum ChallengeCategory {
   exercise('운동'),
   eating('식습관'),
@@ -10,7 +12,13 @@ enum ChallengeCategory {
 
   // JSON에서 객체로 변환
   factory ChallengeCategory.fromJson(String name) {
-    return ChallengeCategory.values.firstWhere((e) => e.name == name);
+    try {
+      return ChallengeCategory.values.firstWhere((e) => e.name == name);
+    } catch (e) {
+      // Log the error and return a default value or handle it appropriately
+      Logger().e('Invalid category name: $name');
+      throw Exception('Invalid category name: $name');
+    }
   }
 
   // 객체에서 JSON으로 변환
