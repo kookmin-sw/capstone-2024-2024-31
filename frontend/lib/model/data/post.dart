d1f68ce9c9d042fbf0e1ff74c4055d7c91782558
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-05-17 15:39:04 +0900

    [backend/feat] 데모

--- frontend/lib/model/data/post.dart
+++ frontend/lib/model/data/post.dart
@@ -1,3 +1,23 @@
+class SimplePost {
+  final int id;
+  final String title;
+  final String author;
+
+  SimplePost({
+    required this.id,
+    required this.title,
+    required this.author,
+  });
+
+  factory SimplePost.fromJson(Map<String, dynamic> json) {
+    return SimplePost(
+      id: json['id'] as int,
+      title: json['title'] as String,
+      author: json['author'] as String,
+    );
+  }
+}
+
 class Author {
   final int id;
   final String email;
