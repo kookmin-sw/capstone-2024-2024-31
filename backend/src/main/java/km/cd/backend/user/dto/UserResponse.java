606600551b6edb3854c17380e76ae9eb2075aad1
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-05-17 09:33:04 +0900

    [backend/fix] 카테고리 필드 추가

--- backend/src/main/java/km/cd/backend/user/dto/UserResponse.java
+++ backend/src/main/java/km/cd/backend/user/dto/UserResponse.java
@@ -1,9 +1,14 @@
 package km.cd.backend.user.dto;
 
+import java.util.List;
+import km.cd.backend.challenge.domain.ChallengeCategory;
+
 public record UserResponse(
         Long id,
         String email,
         String avatar,
         String name,
-        int point
+        int point,
+        List<ChallengeCategory> categories
+        
 ){ }
